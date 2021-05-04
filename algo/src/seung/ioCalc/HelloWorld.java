ed6be365c591ad9f3fea57c3b429254bfaaa3557
Chae HeeSeung <chs4413@gmail.com>
2021-05-04 21:00:07 +0900

    1. 깃헙 클론 후 개인폴더 생성 2. 백준 > 입출력과 사칙연산 > 1 ~ 5번 문제까지 커밋.

---
+++ algo/src/seung/ioCalc/HelloWorld.java
@@ -0,0 +1,13 @@
+package seung.ioCalc;
+
+/*
+ * Hello World!를 출력하시오.
+ * 
+ * @author seung
+ * @since 2021.05.04
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
