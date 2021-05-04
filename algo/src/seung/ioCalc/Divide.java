9a475d1bb11b39783743c07f2787d357651fdc6e
Chae HeeSeung <chs4413@gmail.com>
2021-05-04 23:18:11 +0900

    입출력과 사칙연산 > 6 ~ 9번

--- algo/src/seung/ioCalc/Divide.java
+++ algo/src/seung/ioCalc/Divide.java
@@ -6,13 +6,13 @@ import java.util.Scanner;
  * @author seung
  * @since 2021.05.04
  */
-public class Calc {
+public class Divide {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a, b;
+		double a, b;
 		
-		a = sc.nextInt();
-		b = sc.nextInt();
-		System.out.println(a + b);
+		a = sc.nextDouble();	
+		b = sc.nextDouble();
+		System.out.println(a / b);
 	}
 }
