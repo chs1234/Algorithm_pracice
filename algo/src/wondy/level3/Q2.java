50e60c07efca27563a0e86b6985f2a10ff699bb1
Wondy Kim <wondy96@gmail.com>
2021-06-10 23:46:53 +0900

    3단계 for문
      - 1~11번

---
+++ algo/src/wondy/level3/Q2.java
@@ -0,0 +1,25 @@
+package wondy.level3;
+
+import java.util.Scanner;
+
+public class Q2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		int arr[] = new int[t];
+		
+		for(int i = 0; i < t; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			arr[i] = a + b;
+		}
+		
+		for(int i : arr) {
+			System.out.println(i);			
+		}
+
+	}
+
+}
