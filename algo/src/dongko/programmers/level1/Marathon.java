652cea9adf3c9e5aa29052ad807e3a9e35d71ce8
dongkos <dhkim931101@naver.com>
2021-12-15 17:56:52 +0900

    programmers added 2

---
+++ algo/src/dongko/programmers/level1/Marathon.java
@@ -0,0 +1,31 @@
+package dongko.programmers.level1;
+
+
+/**
+ * @title 완주하지 못한 선수
+ * @author dhkim
+ * @date 2021. 12. 15.
+ * @link https://programmers.co.kr/learn/courses/30/lessons/42576
+ */
+public class Marathon {
+	
+	//javascript로 풀었습니다
+//	function solution(participant, completion) {
+//	    
+//	    var answer = "";
+//	    
+//	    participant.sort();
+//	    completion.sort();
+//	    
+//	    for(var i = participant.length-1; i >= 0; i--) {
+//	      if(participant[participant.length-1] == completion[completion.length-1]) {
+//	          completion.pop();
+//	          participant.pop();
+//	      } else{
+//	          break;
+//	      }
+//	    }
+//	    return participant[participant.length -1];
+//	}
+
+}
